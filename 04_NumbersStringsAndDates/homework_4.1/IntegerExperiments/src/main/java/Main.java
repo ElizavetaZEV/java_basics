dbec22809223915e853f33ec03619dc4358b8d6d
Your Name <lisa-nev@yandex.ru>
2021-04-14 22:48:08 +0700

    IntegerExp

--- 04_NumbersStringsAndDates/homework_4.1/IntegerExperiments/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.1/IntegerExperiments/src/main/java/Main.java
@@ -1,13 +1,13 @@
 public class Main {
-/// проверка для гита
-  public static void main(String[] args) {
-    Container container = new Container();
-    container.count += 7843;
+    /// проверка для гита
+    public static void main(String[] args) {
+        Container container = new Container();
+        container.count += 7843;
 
-    int sum = sumDigits(12345);
-    System.out.println(sumDigits(12345));
+        int sum = sumDigits(12345);
+        System.out.println(sumDigits(12345));
 
-  }
+    }
   /* Реализуйте метод sumDigits который возвращает сумму цифр числа, пример:
   передано 12345, метод должен вернуть 15
   если передано null, то должен вернуть -1
@@ -18,19 +18,38 @@ public class Main {
   В противном случае тестовый метод не сможет проверить ваш код
    */
 
-  public static int sumDigits(Integer number) {
-    //@TODO: write code here and delete TODO line
+    public static int sumDigits(Integer number) {
+        //@TODO: write code here and delete TODO line
 
 
-    Object object = null;
-    String objectString = String.valueOf(object);
-    System.out.println(objectString);
+        Object object = null;
+        String objectString = String.valueOf(object);
+        System.out.println(objectString);
 
-   String[]chislo = String.valueOf(12345).split("");
-int result = 0;
-for (int i = 0; i < chislo.length; i++){
-  result += Integer.parseInt(chislo[i]);
-}
-    return result;
-  }
+        String[] chislo = String.valueOf(12345).split("");
+        int result = 0;
+        for (int i = 0; i < chislo.length; i++) {
+            result += Integer.parseInt(chislo[i]);
+        }
+        return result;
+
+//_____________________________________________
+        String text = "12345";
+        Integer number1 = Integer.parseInt(text);
+        System.out.println(number1);
+        number = number1 + 5;
+        System.out.println(number);
+        System.out.println(text + "000");
+
+
+        //_______________________________________________
+        //String charAt
+        String string = "12345";
+        Character character = string.charAt(0);
+        int index = 1;
+        System.out.println(character);
+        System.out.println(index);
+
+
+    }
 }
