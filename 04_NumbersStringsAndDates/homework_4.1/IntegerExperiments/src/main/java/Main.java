5eafd9494ac249c301f627cb5e40ce7edc0fba11
Your Name <lisa-nev@yandex.ru>
2021-06-07 00:27:35 +0700

    CharacterSum

--- 04_NumbersStringsAndDates/homework_4.1/IntegerExperiments/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.1/IntegerExperiments/src/main/java/Main.java
@@ -32,20 +32,19 @@ public class Main {
 
 
 
-       // String[] chislo = String.valueOf(number).split("");
-       // int result = 0;
-        //for (int i = 0; i < chislo.length; i++) {
-        //    result += Integer.parseInt(chislo[i]);
-       // }
-        //return result;
+       String[] chislo = String.valueOf(number).split("");
+       int result = 0;
+        for (int i = 0; i < chislo.length; i++) {
+            result += Integer.parseInt(chislo[i]);
+        }
+        return result;
 
         //дополнительное задание
-        Integer sum = 0;
-        String string = String.valueOf(number);
-        for (int i1 = 0; i1 < string.length(); i1++){
-            sum += Integer.parseInt(String.valueOf(string.charAt(i1)));
-        }
-        return sum;
+        char c = 12345;
+        int i1 = Character.getNumericValue(c);
+        System.out.println(i1);
+        return number;
+
 
     }
 }
