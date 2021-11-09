fd5042b422edb0a9e21c9f71faf6586e4fe6a2d1
Your Name <lisa-nev@yandex.ru>
2021-11-09 22:13:12 +0700

    char_TAB

--- 04_NumbersStringsAndDates/homework_4.1/IntegerExperiments/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.1/IntegerExperiments/src/main/java/Main.java
@@ -7,6 +7,15 @@ public class Main {
         int sum = sumDigits(12345);
         System.out.println(sumDigits(12345));
 
+        int i1 = 203;
+        char c1 = (char) i1;
+        System.out.println(i1 + " / " + c1);
+
+        for (int i = 1040;   i < 1104;  i++){
+            char c = (char) i;
+            System.out.println(i + " / " + c);
+        }
+
     }
   /* Реализуйте метод sumDigits который возвращает сумму цифр числа, пример:
   передано 12345, метод должен вернуть 15
@@ -22,21 +31,24 @@ public class Main {
         //@TODO: write code here and delete TODO line
 
         //проверка
-        if (number == null){
+        if (number == null) {
             return -1;
         }
 
         Object object = null;
         String objectString = String.valueOf(object);
-            System.out.println(objectString);
+        System.out.println(objectString);
 
 
-            String[] chislo = String.valueOf(number).split("");
-           int result = 0;
-            for (int i = 0; i <= chislo.length; i++) {
+        String[] chislo = String.valueOf(number).split("");
+        int result = 0;
+        for (int i = 0; i <= chislo.length; i++) {
             result += Character.getNumericValue(i);
-            }
-          return result;
+        }
+        return result;
+
+
+
 
     }
 }
