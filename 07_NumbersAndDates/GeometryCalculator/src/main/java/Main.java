f66e24dd1f56389d6342972fbccb9f2927ce63bd
zykova_elizaveta <lisa-nev@yandex.ru>
2022-01-18 16:55:01 +0700

    proverka_branch

--- 07_NumbersAndDates/GeometryCalculator/src/main/java/Main.java
+++ 07_NumbersAndDates/GeometryCalculator/src/main/java/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
+//ПРОВЕРКА
+        public void SguareCircle()
+    {
+
+        double radius = 1;
+
+        System.out.println("Площадь круга: " + Math.PI * (Math.pow(radius, 2)) + " мм.");
+        }
+
+        double a = 5;
+        double b = 8;
+        double c = 7;
+
+
+        //проверка
+        public void isTrianglePossible() {
+            if ((a + b) > c) return true;
+            else if ((a + c) > b) return true;
+            else ((b + c) > a) return true;
+            System.out.println();
+            return false;
+        }
+
+        public double semiPerimeter () {
+            if ((a + b + c) / 2) {
+                System.out.println("Полупериметр треугольника: " +);
+            }
+        }
 
     }
 }
